57d4ecd1efcf595afb2fbffc87a7691fb9984057
Vidushi Gupta <vidushigupta0604@gmail.com>
2026-09-02 01:17:50 +0530

    hashing concept added

--- Hashing/isomorphic_strings (leetcode).py
+++ Hashing/isomorphic_strings (leetcode).py
@@ -1,17 +1,16 @@
-s = "ab"
-t = "cc"
-d1 = {}
-d2 = {}
-for i in range(len(s)):
-    if s[i] in d1:
-        if d1[s[i]] != t[i]:
-            print("false")
-    else:
-        d1[s[i]] = t[i]
-for j in range(len(t)):
-    if t[j] in d2:
-        if d2[t[j]] != s[j]:
-            print("false")
-    else:
-        d2[t[j]] = s[j]
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        d1 = {}
+        d2 = {}
 
+        for i in range(len(s)):
+            if s[i] in d1 and d1[s[i]] != t[i]:
+                return False
+
+            if t[i] in d2 and d2[t[i]] != s[i]:
+                return False
+
+            d1[s[i]] = t[i]
+            d2[t[i]] = s[i]
+
+        return True
--- Hashing/longest_consecutive_sequence (leetcode).py
+++ Hashing/longest_consecutive_sequence (leetcode).py
@@ -1,15 +1,16 @@
-nums = [100,4,200,1,3,2]
-num_set = set(nums)
-longest = 0
-for i in num_set:
-    if i-1 not in num_set:
-        current = i
-        count = 1
-
-        while current + 1 in num_set:
-            current +=1
-            count += 1
-
-        longest = max(longest,count)
-
-print(longest)
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        longest = 0
+        for i in num_set:
+            if i-1 not in num_set:
+                current = i
+                count = 1
+        
+                while current + 1 in num_set:
+                    current +=1
+                    count += 1
+        
+                longest = max(longest,count)
+        
+        return longest
